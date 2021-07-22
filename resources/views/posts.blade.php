a948423c162237ac6e26c3c6cd266655c8f0c9c5
Tashi21 <paritosh.panda_ug23@ashoka.edu.in>
2021-07-22 19:49:04 +0530

    Blade

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,26 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('components/layout')
 
-    <head>
-        <meta charset="UTF-8">
-        <title>My Blog</title>
-        <link rel="stylesheet" href="/app.css">
-    </head>
-
-    <body>
-        @foreach ($posts as $post)
-        <article>
-            <h1>
-                <a href="/posts/{{$post->slug}}">
-                    {{$post->title}}
-                </a>
-            </h1>
-            <div>
-                <p>{!!$post->excerpt!!}</p>
-            </div>
-        </article>
-        @endforeach
-
-    </body>
-
-</html>
+@section('content')
+@foreach ($posts as $post)
+<article>
+    <h1>
+        <a href="/posts/{{$post->slug}}">
+            {{$post->title}}
+        </a>
+    </h1>
+    <div>
+        <p>{!!$post->excerpt!!}</p>
+    </div>
+</article>
+@endforeach
+@endsection
